85bf60d5ce4ffd5972226e49a6da04522d30d73c
dindinet <github.dklongley@neverbox.com>
2016-11-30 08:18:16 +0000

    latest code from dklongley cloudshell

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir public/static
+mv public/*.* public/static
+mv public/js/ public/static
+mv public/img/ public/static
+mv public/css/ public/static
+rm -rf public/base
+cp app.yaml public/app.yaml
+cd public
+appcfg.py update app.yaml  
